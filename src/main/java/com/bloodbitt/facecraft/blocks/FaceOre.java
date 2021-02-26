108fafb9a974180f70d345d93f122ecb9c2db53a
OrangeSniper <51276538+OrangeSniper@users.noreply.github.com>
2021-02-26 10:19:12 -0500

    added ore generation

--- src/main/java/com/bloodbitt/facecraft/blocks/FaceOre.java
+++ src/main/java/com/bloodbitt/facecraft/blocks/FaceOre.java
@@ -1,15 +1,16 @@
 package com.bloodbitt.facecraft.blocks;
 
 import net.minecraft.block.Block;
+import net.minecraft.block.OreBlock;
 import net.minecraft.block.SoundType;
 import net.minecraft.block.material.Material;
 import net.minecraftforge.common.ToolType;
 
-public class FaceOre extends Block
+public class FaceOre extends OreBlock
 {
 
     public FaceOre( ) {
-        super(Properties.create(Material.ROCK)
+        super(Block.Properties.create(Material.ROCK)
                 .hardnessAndResistance(5f, 6f)
                 .sound(SoundType.STONE)
                 .harvestLevel(1)
