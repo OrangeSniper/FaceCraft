7c9d455079e721bb0a0a9c82c017ff9e22044ecd
OrangeSniper <51276538+OrangeSniper@users.noreply.github.com>
2021-02-27 20:50:45 -0500

    started work on entity

---
+++ src/main/java/com/bloodbitt/facecraft/entities/Face_Demon_entity.java
@@ -0,0 +1,14 @@
+package com.bloodbitt.facecraft.entities;
+
+import net.minecraft.entity.AgeableEntity;
+import net.minecraft.entity.CreatureEntity;
+import net.minecraft.entity.EntityType;
+import net.minecraft.world.World;
+
+import javax.annotation.Nullable;
+
+public class Face_Demon_entity extends CreatureEntity {
+    public Face_Demon_entity(EntityType<? extends CreatureEntity> type, World worldIn) {
+        super(type, worldIn);
+    }
+}
