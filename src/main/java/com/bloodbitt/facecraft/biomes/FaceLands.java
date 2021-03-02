56413498b7d5c378f97ca1805dbb0511fdf668b4
OrangeSniper <51276538+OrangeSniper@users.noreply.github.com>
2021-03-02 10:33:13 -0500

    tried to add biome. credits to https://github.com/MysticMods/Traverse for code. not sure if it works yet

---
+++ src/main/java/com/bloodbitt/facecraft/biomes/FaceLands.java
@@ -0,0 +1,13 @@
+package com.bloodbitt.facecraft.biomes;
+
+import com.bloodbitt.facecraft.biomeBuilder.TerraformBiomeBuilder;
+import com.bloodbitt.facecraft.init.ModSurfaceBuilders;
+import net.minecraft.world.biome.Biome;
+import net.minecraft.world.gen.surfacebuilders.ConfiguredSurfaceBuilder;
+import net.minecraft.world.gen.surfacebuilders.SurfaceBuilder;
+
+public class FaceLands {
+    static final BiomeTemplate FACE_LANDS = new BiomeTemplate(TerraformBiomeBuilder.create()
+    .surfaceBuilder(ModSurfaceBuilders.CONFIGURED_FACE_LANDS)
+    .precipitation(Biome.RainType.RAIN));
+}
