8a721a0ac35d910c20e726f7f5aed3503d3466c6
OrangeSniper <51276538+OrangeSniper@users.noreply.github.com>
2021-08-17 09:10:30 -0400

    inverted face works

--- src/main/java/io/github/bloodbitt/facecraft/items/InvertedFace.java
+++ src/main/java/io/github/bloodbitt/facecraft/items/InvertedFace.java
@@ -1,8 +1,15 @@
 package io.github.bloodbitt.facecraft.items;
 
 import net.minecraft.client.util.ITooltipFlag;
+import net.minecraft.entity.Entity;
+import net.minecraft.entity.LivingEntity;
+import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
+import net.minecraft.potion.EffectInstance;
+import net.minecraft.potion.Effects;
+import net.minecraft.util.ActionResult;
+import net.minecraft.util.Hand;
 import net.minecraft.util.text.ITextComponent;
 import net.minecraft.util.text.TranslationTextComponent;
 import net.minecraft.world.World;
@@ -22,5 +29,11 @@ public class InvertedFace extends Item {
     public void addInformation(ItemStack stack, @Nullable World worldIn, List<ITextComponent> tooltip, ITooltipFlag flagIn) {
         super.addInformation(stack, worldIn, tooltip, flagIn);
         tooltip.add(new TranslationTextComponent("tooltip.FaceCraft.InvertedFace"));
+    }
+
+    @Override
+    public boolean hitEntity(ItemStack stack, LivingEntity target, LivingEntity attacker) {
+        target.addPotionEffect(new EffectInstance(Effects.LEVITATION, 100, 100));
+        return super.hitEntity(stack, target, attacker);
     }
 }
